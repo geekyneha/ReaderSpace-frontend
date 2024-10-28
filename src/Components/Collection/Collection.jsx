23121384c27001344774a28f009db4b9ae0c6fa7
Neha <nehatualea@gmail.com>
2024-10-28 23:35:14 +0530

    added: loading msg

--- src/Components/Collection/Collection.jsx
+++ src/Components/Collection/Collection.jsx
@@ -5,46 +5,51 @@ import { useState } from 'react';
 import Navbar from '../Navbar/Navbar.jsx';
 
 const Collection = () => {
-  const [books,setBooks]=useState([]);
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getData = async () => {
     try {
-      const response = await axios.get('https://readerspace-backend.onrender.com/api/books/');
+      const response = await axios.get(
+        'https://readerspace-backend.onrender.com/api/books/'
+      );
       console.log(response.data);
       setBooks(response.data);
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
-  
-   useEffect(()=>{
 
-   getData();
-
-  },[])
-
-
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <div className={styles['container']}>
-       {
-        books.map((book,index)=>(
-          <div className={styles['book-card']} key={index}>
-          <img
-          className={styles['cover-image']}
-            src={book.coverImage}
-            alt="cover-page"
-          />
+        {loading ? (
+          <div className={styles['loading']}> <img src="https://i.sstatic.net/kOnzy.gif" alt="loading" /> Loading...</div>
+        ) : (
+          books.map((book, index) => (
+            <div
+              className={styles['book-card']}
+              key={index}
+            >
+              <img
+                className={styles['cover-image']}
+                src={book.coverImage}
+                alt="cover-page"
+              />
 
-          <h4>{book.title}</h4>
-          <p className={styles['author']}> by : {book.author}</p>
-          <p className={styles['genre']}>{book.genre}</p>
-        </div>
-        ))
-       }
-      
+              <h4>{book.title}</h4>
+              <p className={styles['author']}> by : {book.author}</p>
+              <p className={styles['genre']}>{book.genre}</p>
+            </div>
+          ))
+        )}
       </div>
     </>
   );
